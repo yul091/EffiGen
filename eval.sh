15fa60cfaabe1902133cc52c210dfe275b1ee5a3
yul091 <yul091@ucsd.edu>
2025-02-03 13:20:29 -0800

    mistral

--- eval.sh
+++ eval.sh
@@ -3,7 +3,7 @@
 # parser.add_argument('--capacity', type=int, default=128)
 # parser.add_argument('--longbench_e', action='store_true', help="Evaluate on LongBench-E")
 
-
+# mistralai/Mistral-7B-Instruct-v0.2
 python eval.py \
     --results_dir results-norm-value/results_long_bench_reason_base128_beta1.005_temp1 \
     --model meta-llama-3-8b-instruct \
